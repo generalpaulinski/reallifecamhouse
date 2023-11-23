530505f2adcc1bb27c3c79cd8fe257d7a375d1f5
Pauline Schmiechen <generalpaulinski@users.noreply.github.com>
2023-11-23 17:27:07 +0100

    edits

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'RealLifeCam 24/7 FREE Voyeur',
+  description: 'Watch 24/7 FREE RealLifeCam Voyeur Videos [LIVE] + Replays and Archive from hundreds of voyeur cams, creators and locations!',
 }
 
 import Hero from '@/components/hero'
--- components/hero.tsx
+++ components/hero.tsx
@@ -11,14 +11,14 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Landing template for startups</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+            <h1 className="h1 mb-4" data-aos="fade-up">Watch 24/7 FREE RealLifeCam Voyeur Videos [LIVE]</h1>
+            <p className="text-xl text-gray-200 mb-8" data-aos="fade-up" data-aos-delay="200">Live Cams, Replays and Archive from hundreds of voyeur cams, creators and locations!</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-green-500 hover:bg-green-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Watch more cams</a>
+                <a className="btn text-white bg-green-500 hover:bg-green-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://voyeurhouse.com/live/">Watch more cams</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://voyeurhouse.com/replay/">Learn more</a>
               </div>
             </div>
           </div>
@@ -31,6 +31,12 @@ export default function Hero() {
             video="/videos/video.mp4"
             videoWidth={1920}
             videoHeight={1080} />
+
+          <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center mt-4">
+            <div data-aos="fade-up" data-aos-delay="400">
+              <a className="btn text-white bg-green-500 hover:bg-green-700 w-full mb-8 sm:w-auto sm:mb-0" href="https://voyeurhouse.com/live/">Continue watching</a>
+            </div>
+          </div>
 
         </div>
 
