548747a872557cf02ee68eba4e2f630489a3838e
Pauline Schmiechen <generalpaulinski@users.noreply.github.com>
2023-11-27 13:05:03 +0100

    tracking and gtag

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -6,11 +6,16 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 import Footer from '@/components/ui/footer'
-
 import ReactGA from 'react-ga';
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-WTLQRL9N'
+}
 
 ReactGA.initialize('G-Q3MV2XW4LX');
 
+TagManager.initialize(tagManagerArgs);
 export default function DefaultLayout({
   children,
 }: {
